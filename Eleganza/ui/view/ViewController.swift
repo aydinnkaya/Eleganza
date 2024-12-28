6d6d15bac8c6c6e71a45ac75e627cdddd528176c
aydinnkaya <Aydinnkaya1@gmail.com>
2024-12-28 19:50:45 +0300

    Initial Commit

---
+++ Eleganza/ui/view/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Eleganza
+//
+//  Created by Aydın KAYA on 28.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
