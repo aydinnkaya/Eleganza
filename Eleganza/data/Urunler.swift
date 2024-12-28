0e2d7de32d35bb7850dd447537dcf8100adfeca6
aydinnkaya <Aydinnkaya1@gmail.com>
2024-12-28 20:56:06 +0300

    MainScreenViewCreated

---
+++ Eleganza/data/Urunler.swift
@@ -0,0 +1,8 @@
+//
+//  Urunler.swift
+//  Eleganza
+//
+//  Created by Aydın KAYA on 28.12.2024.
+//
+
+import Foundation
---
+++ Eleganza/ui/ProdottiCellScreenTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProdottiCellScreenTableViewCell.swift
+//  Eleganza
+//
+//  Created by Aydın KAYA on 28.12.2024.
+//
+
+import UIKit
+
+class ProdottiCellScreenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
